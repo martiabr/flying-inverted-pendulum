88b4d4fe8b9aacf9c042412ab61c054d5e61a43b
martiabr <martin.abrandt@outlook.com>
2019-10-29 10:28:39 +0100

    Finished simulink of dynamics

---
+++ fip_init.m
@@ -0,0 +1,18 @@
+g = 9.81;
+L = 1;
+T = 20;
+w = [0 0 0]';
+a = [0 0 10]';
+
+% N = 13;
+% M = 4;
+% h = 0.01;
+% T = 10;
+% K = T / h;
+% 
+% x = zeros(N, K); % x y z x_dot y_dot z_dot gamma beta alpha r s r_dot s_dot
+% u = zeros(M, K); % w_x w_y w_z a
+% 
+% for k = 1:K
+%     
+% end
