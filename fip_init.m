52ced2d5ba171a076bae3d212e7b8d6554c4df41
martiabr <martin.abrandt@outlook.com>
2019-10-31 13:10:42 +0100

    Finished closing loop. LQR manages to stabilize system:D

--- fip_init.m
+++ fip_init.m
@@ -1,10 +1,16 @@
 clc;
+clear;
 
+%% Model parameters:
 g = 9.81;
 L = 1;
 T = 20;
-w = [0 0 0]';
-a = [0 0 10]';
+
+%% Initial values:
+x0 = [0.1; -0.2; 0.5];
+xd0 = [0; 0; 0];
+
+%% LQR gain calculation:
 % x y z xd yd zd gamma beta alpha r s rd sd (13)
 A_vert = [0 1; 0 0]; % z z_d (2)
 B_vert = [0; 1]; % a (1)
@@ -22,12 +28,25 @@ C_lat = eye(5);
 ss_vert = ss(A_vert, B_vert, C_vert, 0);
 ss_lat = ss(A_lat, B_lat, C_lat, 0);
 
-Q_vert = [1 0; 0 0];
+Q_vert = [100 0; 0 0];
 R_vert = 1;
 
-Q_lat = diag([1, 0, 0, 1, 0]);
+Q_lat = diag([10, 0, 0, 100, 0]);
 R_lat = 1;
 
 [K_vert, S_vert, P_vert] = lqr(ss_vert, Q_vert, R_vert);
 [K_lat, S_lat, P_lat] = lqr(ss_lat, Q_lat, R_lat);
 
+
+
+
+
+A_lat2 = [0 1 0 0 0;
+         0 0 -g 0 0;
+         0 0 0 0 0;
+         0 0 0 0 1;
+         0 0 g g/L 0]; % x x_d beta r rd (5)
+B_lat2 = [0; 0; 1; 0; 0]; % w_y
+C_lat2 = eye(5);
+ss_lat2 = ss(A_lat2, B_lat2, C_lat2, 0);
+[K_lat2, S_lat2, P_lat2] = lqr(ss_lat2, Q_lat, R_lat);
