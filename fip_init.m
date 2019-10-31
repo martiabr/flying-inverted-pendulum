22748afb8e48a6d309cb1b1d0ba372ef02865a20
martiabr <martin.abrandt@outlook.com>
2019-10-31 13:33:15 +0100

    Clean up init script

--- fip_init.m
+++ fip_init.m
@@ -12,41 +12,35 @@ xd0 = [0; 0; 0];
 
 %% LQR gain calculation:
 % x y z xd yd zd gamma beta alpha r s rd sd (13)
-A_vert = [0 1; 0 0]; % z z_d (2)
-B_vert = [0; 1]; % a (1)
-C_vert = eye(2);
-A_lat = [0 1 0 0 0;
+A_z = [0 1; 0 0]; % z z_d (2)
+B_z = [0; 1]; % a (1)
+C_z = eye(2);
+ss_z = ss(A_z, B_z, C_z, 0);
+
+A_x = [0 1 0 0 0;
          0 0 g 0 0;
          0 0 0 0 0;
          0 0 0 0 1;
          0 0 -g g/L 0]; % x x_d beta r rd (5)
-B_lat = [0; 0; 1; 0; 0]; % w_y
-C_lat = eye(5);
-% Note that the two lateral controllers will be identical with opposite
-% signs, so only one gain is calculated.
+B_x = [0; 0; 1; 0; 0]; % w_y
+C_x = eye(5);
+ss_x = ss(A_x, B_x, C_x, 0);
 
-ss_vert = ss(A_vert, B_vert, C_vert, 0);
-ss_lat = ss(A_lat, B_lat, C_lat, 0);
-
-Q_vert = [100 0; 0 0];
-R_vert = 1;
-
-Q_lat = diag([10, 0, 0, 100, 0]);
-R_lat = 1;
-
-[K_vert, S_vert, P_vert] = lqr(ss_vert, Q_vert, R_vert);
-[K_lat, S_lat, P_lat] = lqr(ss_lat, Q_lat, R_lat);
-
-
-
-
-
-A_lat2 = [0 1 0 0 0;
+A_y = [0 1 0 0 0;
          0 0 -g 0 0;
          0 0 0 0 0;
          0 0 0 0 1;
          0 0 g g/L 0]; % x x_d beta r rd (5)
-B_lat2 = [0; 0; 1; 0; 0]; % w_y
-C_lat2 = eye(5);
-ss_lat2 = ss(A_lat2, B_lat2, C_lat2, 0);
-[K_lat2, S_lat2, P_lat2] = lqr(ss_lat2, Q_lat, R_lat);
+B_y = [0; 0; 1; 0; 0]; % w_y
+C_y = eye(5);
+ss_y = ss(A_y, B_y, C_y, 0);
+
+Q_z = [100 0; 0 0];
+R_z = 1;
+
+Q_xy = diag([10, 0, 0, 100, 0]);
+R_xy = 1;
+
+[K_z, ~, ~] = lqr(ss_z, Q_z, R_z);
+[K_x, ~, ~] = lqr(ss_x, Q_xy, R_xy);
+[K_y, ~, ~] = lqr(ss_y, Q_xy, R_xy);
